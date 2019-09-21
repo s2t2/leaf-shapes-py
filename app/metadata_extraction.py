d191841c442ac412c525e6f4781cb473c682b7c1
Mike Rossetti <s2t2@users.noreply.github.com>
2019-09-20 20:04:30 -0400

    Loop through img dirs

--- app/metadata_extraction.py
+++ app/metadata_extraction.py
@@ -1,3 +1,20 @@
 
-def message():
-    return "Hello"
+
+import os
+
+IMG_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "img")
+
+#def message():
+#    return "Hello"
+
+if __name__ == "__main__":
+
+    subdirs = os.listdir(IMG_DIRPATH)
+    subdirs.pop(subdirs.index(".gitignore"))
+    subdirs.sort()
+    for dirname in subdirs:
+        dirpath = os.path.join(IMG_DIRPATH, dirname)
+        img_filenames = os.listdir(dirpath)
+        #print("--------------")
+        print(dirname.upper(), f"({len(img_filenames)})")
+        #print(img_filenames)
--- test/metadata_extraction_test.py
+++ test/metadata_extraction_test.py
@@ -1,5 +1,5 @@
 
 from app.metadata_extraction import message
 
-def test_message():
-    assert message() == "Hello"
+#def test_message():
+#    assert message() == "Hello"
