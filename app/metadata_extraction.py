21e59fc2090212aefd02066ecd2ddfd0be0a90df
Mike Rossetti <s2t2@users.noreply.github.com>
2019-09-20 21:17:49 -0400

    Loop through images

--- app/metadata_extraction.py
+++ app/metadata_extraction.py
@@ -2,19 +2,29 @@
 
 import os
 
-IMG_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "img")
+IMG_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "img", "training")
 
-#def message():
-#    return "Hello"
-
-if __name__ == "__main__":
-
+def parse_images():
     subdirs = os.listdir(IMG_DIRPATH)
     subdirs.pop(subdirs.index(".gitignore"))
+    #subdirs.pop(subdirs.index(".DS_Store"))
+    #subdirs.pop(subdirs.index("leaf-shapes.jpg"))
+    #subdirs = [s for s in subdirs if s not in [".gitignore", ".DS_Store"]]
     subdirs.sort()
+    images = []
     for dirname in subdirs:
         dirpath = os.path.join(IMG_DIRPATH, dirname)
         img_filenames = os.listdir(dirpath)
         #print("--------------")
-        print(dirname.upper(), f"({len(img_filenames)})")
-        #print(img_filenames)
+        #print(dirname.upper(), f"({len(img_filenames)})")
+        for img_filename in img_filenames:
+            images.append({"filename": img_filename, "family": dirname})
+    return images
+
+if __name__ == "__main__":
+
+    images = parse_images()
+    print(len(images), "IMAGES:")
+
+    for img in images:
+        print(img["family"].upper(), ">", img["filename"])
