5b3389de99558f31efb72e9e75595cb78e942a5f
Mike Rossetti <s2t2@users.noreply.github.com>
2019-09-30 00:29:03 -0400

    Vision service analyze from url

--- app/vision_service.py
+++ app/vision_service.py
@@ -1,44 +1,49 @@
 # adapted from: https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts/python-analyze
-
-import os
-import json
-# from pprint import pprint
-
+from pdb import set_trace as breakpoint
+from pprint import pprint
 from dotenv import load_dotenv
+import os
 import requests
+import json
 
 load_dotenv()
 
 API_KEY = os.getenv("VISION_API_KEY", default="OOPS")
 API_ENDPOINT = os.getenv("VISION_API_ENDPOINT", default="OOPS")
 
-request_url = API_ENDPOINT + "vision/v2.0/analyze"
-headers = {'Ocp-Apim-Subscription-Key': API_KEY}
-params = {'visualFeatures': 'Categories,Description,Color'}
-image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
-request_body = {'url': image_url}
-response = requests.post(request_url, headers=headers, params=params, json=request_body)
-response.raise_for_status()
 
-# The 'analysis' object contains various fields that describe the image. The most
-# relevant caption for the image is obtained from the 'description' property.
-analysis = response.json()
-print(json.dumps(response.json()))
-image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+def analyze_image_url(image_url):
+    request_url = f"{API_ENDPOINT}/vision/v2.0/analyze"
+    headers = {"Ocp-Apim-Subscription-Key": API_KEY}
+    params = {"visualFeatures": "Categories,Description,Color"}
+    request_body = {"url": image_url}
+
+    response = requests.post(request_url, headers=headers, params=params, json=request_body)
+    response.raise_for_status() # does this raise if status not 200?
+
+    parsed_response = json.loads(response.text)
+    return parsed_response
+
+if __name__ == "__main__":
+
+    example_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
+    print("IMAGE URL", example_url)
+
+    parsed_response = analyze_url(example_url)
+    pprint(parsed_response)
+
+    #breakpoint()
 
 
-
-exit()
-
-#from io import BytesIO
-#from PIL import Image
-## If you are using a Jupyter notebook, uncomment the following line.
-## %matplotlib inline
-#import matplotlib.pyplot as plt
-#
-## Display the image and overlay it with the caption.
-#image = Image.open(BytesIO(requests.get(image_url).content))
-#plt.imshow(image)
-#plt.axis("off")
-#_ = plt.title(image_caption, size="x-large", y=-0.1)
-#plt.show()
+    #from io import BytesIO
+    #from PIL import Image
+    ## If you are using a Jupyter notebook, uncomment the following line.
+    ## %matplotlib inline
+    #import matplotlib.pyplot as plt
+    #
+    ## Display the image and overlay it with the caption.
+    #image = Image.open(BytesIO(requests.get(image_url).content))
+    #plt.imshow(image)
+    #plt.axis("off")
+    #_ = plt.title(image_caption, size="x-large", y=-0.1)
+    #plt.show()
--- test/vision_service_test.py
+++ test/vision_service_test.py
@@ -1,4 +1,14 @@
+import os
+import pytest
 
+from app.vision_service import analyze_image_url
 
+CI = os.environ.get("CI") == "true"
 
-# todo
+@pytest.mark.skipif(CI, reason="1) prevent HTTP requests on CI, 2) credentials not on CI")
+def test_analyze_image_url():
+    example_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
+    parsed_response = analyze_image_url(example_url)
+    assert list(parsed_response.keys()) == [
+        "categories", "color", "description", "requestId", "metadata"
+    ]
